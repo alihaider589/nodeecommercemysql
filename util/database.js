f3d51d19db66b3f5808e5066b7fa4ef7d8703e9f
alihaider589 <alihaider589@outlook.com>
2021-04-17 17:42:38 +0500

    Adding inital files

---
+++ util/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'nodeecommerce',
+})
+
+
+module.exports = pool.promise();
