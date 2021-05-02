3bd10c6217b56b9fce94e91ddbc21423f22ba3e4
alihaider589 <alihaider589@outlook.com>
2021-05-02 15:43:52 +0500

    Adding product model

--- util/database.js
+++ util/database.js
@@ -1,11 +1,22 @@
-const mysql = require('mysql2')
+// const mysql = require('mysql2')
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'nodeecommerce',
-})
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'nodeecommerce',
+// })
 
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('nodeecommerce', 'root', 'root',
+    {
+        dialect: 'mysql',
+        host: 'localhost'
+    })
+
+    module.exports= sequelize;
